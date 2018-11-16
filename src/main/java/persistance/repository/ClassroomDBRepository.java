34c26ce33d310df9c34ff64a467061e8c0e7ca6b
michal.mionskowski <michal.mionskowski95@outlook.com>
2018-11-16 09:07:36 +0000

    Persistance feature finished

--- src/main/java/persistance/repository/ClassroomDBRepository.java
+++ src/main/java/persistance/repository/ClassroomDBRepository.java
@@ -4,11 +4,14 @@ import static javax.transaction.Transactional.TxType.SUPPORTS;
 
 import javax.enterprise.inject.Default;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import persistance.domain.Classroom;
+import util.JSONUtil;
 @Default
 @Transactional(SUPPORTS)
 @RequestScoped
@@ -16,34 +19,42 @@ public class ClassroomDBRepository implements ClassroomRepository{
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
 	
+	@Inject
+	JSONUtil JSONUtil;
 	public Classroom findClassroom(int classroomID) {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Classroom> query = em.createQuery("SELECT c FROM Classroom c WHERE c.classroomID='" + classroomID + "'", Classroom.class);
+		return query.getSingleResult();
 	}
 
 	public String createClassroom(Classroom newClassroom) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(newClassroom);
+		return JSONUtil.getJSONForObject(em.find(Classroom.class,newClassroom.getClassroomID()));
+	
 	}
 
 	public boolean updateClassroom(Classroom updateClassroom, int classroomIdToChange) {
-		// TODO Auto-generated method stub
-		return false;
+		Classroom oldClassroom = em.find(Classroom.class, classroomIdToChange);
+		oldClassroom.setTrainer((updateClassroom.getTrainer()));
+		oldClassroom.setTrainees(updateClassroom.getTrainees());
+		em.merge(oldClassroom);
+		return em.find(Classroom.class,oldClassroom.getClassroomID())!= null ? true : false;
+	
 	}
 
 	public boolean deleteClassroom(int classroomID) {
-		// TODO Auto-generated method stub
-		return false;
+		em.remove(em.find(Classroom.class, classroomID));
+		return em.find(Classroom.class,em.contains(em.find(Classroom.class, classroomID)))== null ? true : false;
 	}
 
 	public String getAllClassrooms() {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Classroom> query = em.createQuery("SELECT c FROM Classroom c", Classroom.class);
+		return JSONUtil.getJSONForObject(query.getResultList());
+	
 	}
 
 	public String getAllTrainees(int classroomID) {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Classroom> query = em.createQuery("SELECT c.trainees FROM Classroom c WHERE c.classroomID='" + classroomID + "'", Classroom.class);
+		return JSONUtil.getJSONForObject(query.getResultList());
 	}
 
 }
